085a03e3f0ec755c6a10b2946affa915f04383ba
우가빈 <gabeenwoo@gmail.com>
2022-09-13 11:45:30 +0900

    Add Node Network

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -11,16 +11,20 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
+	openPort := r.URL.Query().Get("openPort")
+	ip := utils.Splitter(r.RemoteAddr, ":", 0)
+
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+		return openPort != "" || ip != ""
 	}
+
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, "x", "x")
+	initPeer(conn, ip, openPort)
 }
 
-func AddPeer(address, port string) {
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws", address, port), nil)
+func AddPeer(address, port, openPort string) {
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
 }
