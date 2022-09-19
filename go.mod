dcf7c7932bc437c9e776cfd6b74d475af953eb14
우가빈 <gabeenwoo@gmail.com>
2022-09-19 10:03:36 +0900

    Finalize

--- go.mod
+++ go.mod
@@ -8,4 +8,4 @@ require (
 	go.etcd.io/bbolt v1.3.6
 )
 
-require golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
+require golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/WoodoCoin/cli"
 	"github.com/WoodoCoin/db"
+	"github.com/WoodoCoin/explorer"
 )
 
 func main() {
 	defer db.Close()
 	cli.Start()
+	explorer.Start(7070)
 }
